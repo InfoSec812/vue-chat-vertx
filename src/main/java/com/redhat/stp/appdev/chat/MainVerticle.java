998dd4add741c7aae7db1ae0384f3ed6911b72d9
Deven Phillips <deven.phillips@redhat.com>
2019-10-28 11:52:53 -0400

    Add health check

--- src/main/java/com/redhat/stp/appdev/chat/MainVerticle.java
+++ src/main/java/com/redhat/stp/appdev/chat/MainVerticle.java
@@ -27,13 +27,16 @@ public class MainVerticle extends AbstractVerticle {
       .addOutboundPermitted(new PermittedOptions().setAddress("messages"));
     SockJSHandler sockJsBridge = SockJSHandler.create(vertx);
     sockJsBridge.bridge(bridgeOpts);
+    router.get("/health").handler(ctx -> {
+      ctx.response().setStatusCode(200).setStatusMessage("OK").end("{ \"status\": \"OK\" }");
+    });
     router.route().handler(StaticHandler.create("webroot"));
     router.route("/eventbus/*").handler(sockJsBridge);
-    return vertx.createHttpServer()
-                .requestHandler(router)
-                .rxListen(8080, "0.0.0.0")
-                .doOnError(Completable::error)
-                .toFlowable()
-                .ignoreElements();
+    return vertx.createHttpServer()               // Create HttpServer instance
+                .requestHandler(router)           // Attach router to handle all requests
+                .rxListen(8080, "0.0.0.0")        // Start the server listening on port 8080
+                .doOnError(Completable::error)    // return an error if there is a problem
+                .toFlowable()                     // Convert to a Flowable so that the Completable can be returned
+                .ignoreElements();                // Return Completable on success
   }
 }
