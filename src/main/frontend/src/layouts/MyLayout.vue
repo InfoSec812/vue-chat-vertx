df854158189133eb25bc6ffdcb00607c097466f3
Deven Phillips <deven.phillips@redhat.com>
2019-10-28 10:11:04 -0400

    Fixed vertx evenbus client issues

--- src/main/frontend/src/layouts/MyLayout.vue
+++ src/main/frontend/src/layouts/MyLayout.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <div class="header">
-      STP Chat
-    </div>
+    <div class="header">STP Chat</div>
     <div class="bodyContent" ref="scrollArea">
       <q-chat-message
         v-for="(msg, index) in sortedMessages"
@@ -22,13 +20,13 @@
         @change="sendMessage"
         style="width: 90vw;"
       />
-      <q-btn icon="send" @click="sendMessage" dense style="width: 30px;"></q-btn>
+      <q-btn icon="send" @click="sendMessage" dense style="width: 30px;" />
     </div>
   </div>
 </template>
 
 <script>
-import EventBus from 'vertx3-eventbus-client';
+import EventBus from "vertx3-eventbus-client";
 
 export default {
   name: "MyLayout",
@@ -49,20 +47,20 @@ export default {
     }
   },
   filters: {
-    dateTime: (value) => {
+    dateTime: value => {
       let dateObject = new Date(value);
       let currentTime = new Date();
       if (dateObject.getDate() === currentTime.getDate()) {
         if (dateObject.getHours() === currentTime.getHours()) {
           if (dateObject.getMinutes() === currentTime.getMinutes()) {
-            return "a few seconds"
+            return "a few seconds";
           } else {
             let minDiff = currentTime.getMinutes() - dateObject.getMinutes();
-            return "~" + minDiff + " minutes ago"
+            return "~" + minDiff + " minutes ago";
           }
         } else {
           let hoursDiff = currentTime.getHours() - dateObject.getHours();
-          return "~" + hoursDiff + " hours ago"
+          return "~" + hoursDiff + " hours ago";
         }
       } else {
         return dateObject.toLocaleString();
@@ -74,18 +72,26 @@ export default {
       const holder = this.messageinput;
       if (this.messageinput.length > 0) {
         this.messageinput = "";
-        let timestamp = (new Date()).toISOString();
-        this.eventBus.send("messages", {msg: holder, timestamp: timestamp, username: this.username, avatar: this.userhash, sent: false});
+        let timestamp = new Date().toISOString();
+        this.eventBus.publish("messages", {
+          msg: holder,
+          timestamp: timestamp,
+          username: this.username,
+          avatar: this.userhash,
+          sent: false
+        });
       }
       this.$refs.editorInput.focus();
     },
     gravatar: () => {
       let length = 32;
-      let result           = '';
-      let characters       = '1234567890abcdef';
+      let result = "";
+      let characters = "1234567890abcdef";
       let charactersLength = characters.length;
-      for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
       }
       return "https://www.gravatar.com/avatar/" + result + "?d=robohash";
     }
@@ -93,15 +99,18 @@ export default {
   watch: {
     messageList: {
       deep: true,
-      handler () {
+      handler() {
         this.$nextTick(() => {
           this.$refs.scrollArea.scrollTop = this.$refs.scrollArea.scrollHeight;
         });
       }
     }
   },
-  created: function() {
-    let eventBusUrl = window.location.protocol + "//" + window.location.host + ":" + window.location.port + "/eventbus";
+  mounted() {
+    this.userhash = this.gravatar();
+    let eventBusUrl = window.location.protocol + "//" + window.location.host + "/eventbus";
+    // let eventBusUrl = window.location.protocol + "//localhost:8080/eventbus";
+    console.log("Initializing event bus bridge: " + eventBusUrl);
     let options = {
       vertxbus_reconnect_attempts_max: Infinity, // Max reconnect attempts
       vertxbus_reconnect_delay_min: 1000, // Initial delay (in ms) before first reconnect attempt
@@ -112,65 +121,60 @@ export default {
     this.eventBus = new EventBus(eventBusUrl, options);
     this.eventBus.enableReconnect(true);
 
-    console.log("Initializing event bus bridge");
     this.eventBus.onopen = () => {
       console.log("Event bus bridge open");
-      this.eventBus.registerHandler("messages", (error, data) => {
-        console.log("Event bus message received: " + JSON.stringify(data));
+      this.eventBus.registerHandler("messages", (error, res) => {
         if (error === null) {
-          if (data.avatar !== this.userhash) {
-            data['sent'] = true;
-            this.messageList.push(data);
+          res.body["sent"] = true;
+          if (res.body.avatar === this.userhash) {
+            res.body["sent"] = false;
           }
+          this.messageList.push(res.body);
           if (this.messageList.length > 200) {
             this.messageList.shift();
           }
         } else {
-          console.log("Error connecting to event bus bridge: " + JSON.stringify(error));
+          console.log(
+            "Error connecting to event bus bridge: " + JSON.stringify(error)
+          );
         }
       });
-    }
-  },
-  mounted() {
-    this.userhash = this.gravatar();
-    window.setInterval(() => {
-      this.currentTime = new Date();
-    }, 10000);
+    };
   }
 };
 </script>
 
 <style>
-  body {
-    overflow: hidden;
-  }
-  .header {
-    position: fixed;
-    padding-left: 10px;
-    padding-top: 6px;
-    z-index: 2;
-    top: 0px;
-    height: 60px;
-    width: 100%;
-    background-color: rgb(238, 0, 0);
-    box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, .2);
-    font-family: Overpass sans-serif;
-    font-size: 26pt;
-    font-weight: bold;
-    color: white;
-  }
-  .footer {
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    height: 100px;
-    box-shadow: -5px -5px 5px -5px rgba(0, 0, 0, .2);
-  }
-  .bodyContent {
-    position: absolute;
-    top: 60px;
-    bottom: 110px;
-    overflow: auto;
-    width: 100vw;
-  }
+body {
+  overflow: hidden;
+}
+.header {
+  position: fixed;
+  padding-left: 10px;
+  padding-top: 6px;
+  z-index: 2;
+  top: 0px;
+  height: 60px;
+  width: 100%;
+  background-color: rgb(238, 0, 0);
+  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.2);
+  font-family: Overpass sans-serif;
+  font-size: 26pt;
+  font-weight: bold;
+  color: white;
+}
+.footer {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 100px;
+  box-shadow: -5px -5px 5px -5px rgba(0, 0, 0, 0.2);
+}
+.bodyContent {
+  position: absolute;
+  top: 140px;
+  bottom: 110px;
+  overflow: auto;
+  width: 100vw;
+}
 </style>
